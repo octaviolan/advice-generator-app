969513d083fc7423367e605db5af1587dab8c9d0
octaviolan <octavio.olan25@gmail.com>
2023-08-09 15:43:13 -0600

    fetch - then para consumir API

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@ const btnGenerator = document.querySelector('.advice__button');
 const advice = document.querySelector('.advice__text');
 const adviceId = document.querySelector('.advice__id');
 
-//usando ajax
+/*usando ajax
 btnGenerator.addEventListener('click', ()=> {
 
   let xhr = new XMLHttpRequest();
 
-  xhr.open('GET', '	https://api.adviceslip.com/advice');
+  xhr.open('GET', 'https://api.adviceslip.com/advice');
 
   xhr.onreadystatechange = ()=> {
     if(xhr.readyState === 4 && xhr.status === 200) {
@@ -15,9 +15,30 @@ btnGenerator.addEventListener('click', ()=> {
       let jsObject = JSON.parse(jsonData);
       console.log(jsObject);
       adviceId.textContent = jsObject.slip.id;
-      advice.textContent = `"${jsObject.slip.advice}"`;
+      advice.textContent = `“${jsObject.slip.advice}”`;
     }
   };
 
   xhr.send();
+})
+*/
+
+//usando fetch - then
+btnGenerator.addEventListener('click', ()=> {
+
+  fetch('https://api.adviceslip.com/advice')
+    .then((response) => {
+      if(!response.ok) {
+        throw new Error("Error")
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      adviceId.textContent = data.slip.id;
+      advice.textContent = `“${data.slip.advice}”`;
+    })
+    .catch((error) =>{
+      advice.textContent = error;
+    })
 })
